9de905ee2cb27957d30634da8c3b784e90b1ae72
Yanaput <nokg957@gmail.com>
2024-05-17 11:33:37 +0700

    add Ex4

---
+++ src/main/java/Ex4_6481145/Main.java
@@ -0,0 +1,17 @@
+//Yanaput Makbonsonglop 6481145
+package Ex4_6481145;
+
+class Seafood implements Comparable<Seafood> {
+    private String name, type;
+    private int omega3, cholesterol;
+    private double mercury;
+    public int compareTo(Seafood other) {
+    // add your code according to conditions in 2.2-2.5
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
